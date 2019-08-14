b0fb08de101f9f92364cdb001015858d96f2da2f
Claire McCarthy <clmccart@microsoft.com>
2019-08-14 13:26:51 -0700

    refactor using fakes

--- testdoubles/fakes/html_pages.py
+++ testdoubles/fakes/html_pages.py
@@ -1,11 +1,18 @@
 import html as html_converter
 
-class HtmlPagesConverter:
+class FileAccessWrapper:
     def __init__(self, filename):
-        """Read the file and note the positions of the page breaks so we can access them..."""
         self.filename = filename
+
+    def open(self):
+        return open(self.filename, "r", encoding="UTF-8")
+        
+class HtmlPagesConverter:
+    def __init__(self, file_access):
+        """Read the file and note the positions of the page breaks so we can access them..."""
         self.breaks = [0]
-        with open(self.filename, "r", encoding="UTF-8") as f:
+        self.file_access = file_access
+        with self.file_access.open() as f:
             while True:
                 line = f.readline()
                 if not line:
@@ -15,19 +22,32 @@ class HtmlPagesConverter:
                     page_break_position = f.tell()
                     self.breaks.append(f.tell())
             self.breaks.append(f.tell())
-    
+
     def get_html_page(self, page):
         """Return html page with the given number (zero indexed)"""
-        page_start = self.breaks[page]
-        page_end = self.breaks[page+1]
-        html = ""
-        with open(self.filename, "r", encoding="UTF-8") as f: 
+        page_start, page_end = self.get_page_bookends(page)
+        
+        with self.file_access.open() as f: 
             f.seek(page_start)
             while f.tell() != page_end:
                 line = f.readline()
                 line = line.rstrip()
                 if "PAGE_BREAK" in line:
                     continue
-                html += html_converter.escape(line, quote=True)
-                html += "<br />"
+                html = self.convert_to_html(line)
+
         return html
+
+    def get_page_bookends(self, page):
+        page_start = self.breaks[page]
+        page_end = self.breaks[page+1]
+        
+        return (page_start, page_end)
+
+    def convert_to_html(self, line):
+        html = ""
+        html += html_converter.escape(line, quote=True)
+        html += "<br />"
+        
+        return html
+   
--- testdoubles/fakes/test_html_pages.py
+++ testdoubles/fakes/test_html_pages.py
@@ -1,13 +1,35 @@
 import unittest
 import os 
 import tempfile
-from testdoubles.fakes.html_pages import HtmlPagesConverter 
+from testdoubles.fakes.html_pages import HtmlPagesConverter, FileAccessWrapper 
+
+import io
+
 class HtmlPagesTest(unittest.TestCase):
     def test_inserts_br_tags_for_linebreaks(self):
-        filename = os.path.join(tempfile.gettempdir(), "afile.txt")
-        f = open(filename, "w", encoding="UTF-8")
-        f.write("plain text\n")
-        f.close()
-        converter = HtmlPagesConverter(filename)
+        converter = HtmlPagesConverter(FakeFileWrapper("plain text\n"))
         new_test = converter.get_html_page(0)
         self.assertEqual("plain text<br />", new_test)
+
+    def test_quotes_escaped(self):
+        converter = HtmlPagesConverter(FakeFileWrapper("text with 'quotes'"))
+        new_text = converter.get_html_page(0)
+        self.assertEqual("text with &#x27;quotes&#x27;<br />", new_text)
+
+    def test_random_access_pages(self):
+        converter = HtmlPagesConverter(FakeFileWrapper("page one\nPAGE_BREAK\npage two\nPAGE_BREAK\n"))
+        page_two = converter.get_html_page(1)
+        self.assertEqual("page two<br />", page_two)
+    
+    def test_html_conversion(self):
+        converter = HtmlPagesConverter(FakeFileWrapper("page one\nPAGE_BREAK\npage two\nPAGE_BREAK\n")) 
+        line = "page two"
+        html = converter.convert_to_html(line)
+        self.assertEqual("page two<br />", html)
+
+class FakeFileWrapper:
+    def __init__(self, text):
+        self.text = text
+    
+    def open(self):
+        return io.StringIO(self.text)
