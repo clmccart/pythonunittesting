91efd389c388c213b7f7cec8f4b8ad8e31a92801
Claire McCarthy <clmccart@microsoft.com>
2019-08-14 12:43:35 -0700

    Get html tests to pass

--- testdoubles/fakes/html_pages.py
+++ testdoubles/fakes/html_pages.py
@@ -1,9 +1,10 @@
 import html as html_converter
 
-class HTmlPagesConverter:
+class HtmlPagesConverter:
     def __init__(self, filename):
         """Read the file and note the positions of the page breaks so we can access them..."""
-        self.filename = filenameself.breaks = [0]
+        self.filename = filename
+        self.breaks = [0]
         with open(self.filename, "r", encoding="UTF-8") as f:
             while True:
                 line = f.readline()
@@ -15,18 +16,18 @@ class HTmlPagesConverter:
                     self.breaks.append(f.tell())
             self.breaks.append(f.tell())
     
-            def get_html_page(self, page):
-                """Return html page with the given number (zero indexed)"""
-                page_start = self.breaks[page]
-                page_end = self.breaks[page+1]
-                html = ""
-                with open(self.filename, "r", encoding="UTF-8") as f: 
-                    f.seek(page_start)
-                    while f.tell() != page_end:
-                        line = f.readline()
-                        line = line.rstrip()
-                        if "PAGE_BREAK" in line:
-                            continue
-                        html += html_converter.escape(line, quote=True)
-                        html += "<br />"
-                return html
+    def get_html_page(self, page):
+        """Return html page with the given number (zero indexed)"""
+        page_start = self.breaks[page]
+        page_end = self.breaks[page+1]
+        html = ""
+        with open(self.filename, "r", encoding="UTF-8") as f: 
+            f.seek(page_start)
+            while f.tell() != page_end:
+                line = f.readline()
+                line = line.rstrip()
+                if "PAGE_BREAK" in line:
+                    continue
+                html += html_converter.escape(line, quote=True)
+                html += "<br />"
+        return html
--- testdoubles/fakes/test_html_pages.py
+++ testdoubles/fakes/test_html_pages.py
@@ -1,10 +1,12 @@
 import unittest
-
+import os 
+import tempfile
+from testdoubles.fakes.html_pages import HtmlPagesConverter 
 class HtmlPagesTest(unittest.TestCase):
     def test_inserts_br_tags_for_linebreaks(self):
         filename = os.path.join(tempfile.gettempdir(), "afile.txt")
         f = open(filename, "w", encoding="UTF-8")
-        f.write("plain test\n")
+        f.write("plain text\n")
         f.close()
         converter = HtmlPagesConverter(filename)
         new_test = converter.get_html_page(0)
