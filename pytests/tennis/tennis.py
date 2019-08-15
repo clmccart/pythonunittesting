10312ef08bed188be7e0f62109150a0336f0ca18
Claire McCarthy <clmccart@microsoft.com>
2019-08-15 14:21:27 -0700

    Add code coverage

--- pytests/tennis/tennis.py
+++ pytests/tennis/tennis.py
@@ -1,8 +1,53 @@
-score_names = ["Love", "Fifteen", "Thirty", "Forty"]
-
 def tennis_score(player1_points, player2_points):
-    if player1_points == player2_points:
-        return "{0}-All".format(score_names[player1_points])
-    else:
-        return "{0}-{1}".format(score_names[player1_points], score_names[player2_points])
+    game = TennisGame("Claire", "Farley")
+    game.player1_points = player1_points
+    game.player2_points = player2_points
+    return game.score()
+
+class TennisGame:
+    def __init__(self, player1_name, player2_name):
+        self.player1_name = player1_name
+        self.player2_name = player2_name
+        self.player1_points = 0
+        self.player2_points = 0
+
+    def score(self):
+        result = ""
+        temp_score = 0
+        if (self.player1_points == self.player2_points):
+            result = {
+                0: "Love-All",
+                1: "Fifteen-All",
+                2: "Thirty-All"
+            }.get(self.player1_points, "Duece")
+        elif (self.player1_points >= 4 or self.player2_points >= 4):
+            minus_result = self.player1_points - self.player2_points
+            if (minut_result == 1):
+                result = "Advantage " + self.player1_name
+            elif (minus_result == -1):
+                result = "Advantage " + self.player2_name
+            elif (minus_result >= 2):
+                result = "Win for " + self.player1_name
+            else: 
+                result = "Win for " + self.player2_name
+        else:
+            for i in range(1,3):
+                if (i==1):
+                    temp_score = self.player1_points
+                else: 
+                    result += "-"
+                    temp_score = self.player2_points
+                result += {
+                    0: "Love",
+                    1: "Fifteen",
+                    2: "Thirty",
+                    3: "Forty"
+                }[temp_score]
+        return result
+
+    def won_point(self, player_name):
+        if player_name == self.player1_name:
+            self.player1_points += 1
+        else:  
+            self.player2_points += 1
         
--- tennis/tennis.py
+++ tennis/tennis.py
@@ -5,4 +5,3 @@ def tennis_score(player1_points, player2_points):
         return "{0}-All".format(score_names[player1_points])
     else:
         return "{0}-{1}".format(score_names[player1_points], score_names[player2_points])
-        
