fe55992f4efbe71a3651e883891f0ce36a026371
Claire McCarthy <clmccart@microsoft.com>
2019-08-15 09:38:35 -0700

    Monkeypatching

--- testdoubles/stubs/test_alarm.py
+++ testdoubles/stubs/test_alarm.py
@@ -1,7 +1,7 @@
 import unittest
 from testdoubles.stubs.alarm import Alarm
 from testdoubles.stubs.stub_sensor import TestSensor
-from unittest.mock import Mock 
+from unittest.mock import * 
 from testdoubles.stubs.sensor import Sensor
 
 class AlarmTest(unittest.TestCase):
@@ -31,3 +31,21 @@ class AlarmTest(unittest.TestCase):
         alarm = Alarm(test_sensor)
         alarm.check()
         self.assertFalse(alarm.is_alarm_on)
+
+    def test_check_with_too_high_pressure_monkeypatching(self):
+        with patch('testdoubles.stubs.alarm.Sensor') as test_sensor_class:
+            test_sensor_instance = Mock()
+            test_sensor_instance.sample_pressure.return_value = 22
+            test_sensor_class.return_value = test_sensor_instance
+            alarm = Alarm()
+            alarm.check()
+            self.assertTrue(alarm.is_alarm_on)
+    
+    @patch("testdoubles.stubs.alarm.Sensor")
+    def test_check_with_too_low_pressure_monkeypatching_decorator(self, test_sensor_class):
+        test_sensor_instance = Mock()
+        test_sensor_instance.sample_pressure.return_value = 15
+        test_sensor_class.return_value = test_sensor_instance
+        alarm = Alarm()
+        alarm.check()
+        self.assertTrue(alarm.is_alarm_on)
